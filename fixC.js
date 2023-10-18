44206ec6618ce77b61e9c09aebac68392cf08a5e
David Benepe <benepe96@gmail.com>
2023-10-18 18:24:43 -0500

    Update fixC.js

--- fixC.js
+++ fixC.js
@@ -3,6 +3,7 @@ const REGEX_ELSE_SAME_LINE = /\}\s*else\s*\{/gm;
 const REGEX_EMPTY_SAME_LINE = /\{\s*\}/gm;
 const REGEX_REMOVE_LEADING_SPACES_LINE = /^ +/gm;
 const REGEX_FIND_SWITCH_BLOCKS = /(?:(?:case)|(?:default)).*\n(?: *)((?:.*\n)*?\s*(?:(?=(?:case))|(?:break;)))/g;
+const REGEX_GET_LEFT_POINTERS = /([a-zA-Z_][a-zA-Z0-9_]*)([*]+)[ ]+([a-zA-Z_][a-zA-Z0-9_]*)/gm;
 
 function _get_detailed_matches(inputText, regex) {
     let results = [];
@@ -17,7 +18,7 @@ function _get_detailed_matches(inputText, regex) {
         
         // The result can be accessed through the `m`-variable.
         m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
+            //console.log(`Found match, group ${groupIndex}: ${match}`);
             matchedStuff[groupIndex] = match;
         });
         
@@ -57,12 +58,22 @@ function _add_indents(input, indentSize = 4) {
     }).join('\n');
 }
 
+function _fix_pointer_alignment(input) {
+    let matches = _get_detailed_matches(input, REGEX_GET_LEFT_POINTERS);
+    for (let match of matches) {
+        let newPattern = match[1] + ' ' + match[2] + match[3];
+        input = input.replace(match[0], newPattern);
+    }
+    return input;
+}
+
 function fix_code_formatting(input) {
     out = input;
     out = _replace_matches(out, _get_whole_matches(out, REGEX_CURLY_BRACE_SAME_LINE), ") {");
     out = _replace_matches(out, _get_whole_matches(out, REGEX_ELSE_SAME_LINE), "} else {");
     out = _replace_matches(out, _get_whole_matches(out, REGEX_EMPTY_SAME_LINE), "{}");
     out = _replace_matches(out, _get_whole_matches(out, REGEX_REMOVE_LEADING_SPACES_LINE), "");
+    out = _fix_pointer_alignment(out);
     out = _add_indents(out);
     return out;
 }
